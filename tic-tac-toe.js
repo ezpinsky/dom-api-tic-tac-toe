9c83d20cb2d4ddc675e6007de662a029f68aef9a
Ezra Pinsky <ezpinsky@gmail.com>
2020-09-03 19:14:23 -0400

    created player click tracker func to switch bet x and o

---
+++ tic-tac-toe.js
@@ -0,0 +1,26 @@
+window.addEventListener("DOMContentLoaded", (event) => {
+	const playerClicks = () => {
+		let clickCount = 0;
+		const grid = document.getElementById("tic-tac-toe-board");
+		const squares = grid.querySelectorAll("div");
+		squares.forEach((square) => {
+			square.addEventListener("click", (event) => {
+				if (clickCount % 2 === 0 && square.innerHTML === "") {
+					console.log(square.innerHTML);
+					//use X picture
+					square.innerHTML = `<img src="https://assets.aaonline.io/Module-DOM-API/formative-project-tic-tac-toe/player-x.svg" id="x-${clickCount}">`;
+					square.classList.add("x");
+					clickCount++;
+				} else if (clickCount % 2 !== 0 && square.innerHTML === "") {
+					console.log(square.innerHTML);
+					//use O picture
+					square.innerHTML = `<img src="https://assets.aaonline.io/Module-DOM-API/formative-project-tic-tac-toe/player-o.svg" id="x-${clickCount}">`;
+					square.classList.add("o");
+					clickCount++;
+				}
+			});
+		});
+	};
+
+	clickTracker();
+});
