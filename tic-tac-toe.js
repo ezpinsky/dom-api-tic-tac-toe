075fd3a4cb9f4e249b8a781552e8056e6d1b2020
Thanh Nguyen <thanhnguyenttn@gmail.com>
2020-09-04 13:43:01 -0400

    added array variable

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,5 +1,7 @@
 window.addEventListener("DOMContentLoaded", (event) => {
-	const playerClicks = () => {
+  const currentGrid = ["", "", "", "", "", "", "", "",""];
+
+  const playerClicks = () => {
 		let clickCount = 0;
 		const grid = document.getElementById("tic-tac-toe-board");
 		const squares = grid.querySelectorAll("div");
@@ -9,18 +11,26 @@ window.addEventListener("DOMContentLoaded", (event) => {
 					console.log(square.innerHTML);
 					//use X picture
 					square.innerHTML = `<img src="https://assets.aaonline.io/Module-DOM-API/formative-project-tic-tac-toe/player-x.svg" id="x-${clickCount}">`;
-					square.classList.add("x");
-					clickCount++;
+          square.classList.add("x");
+          let squareNum = (square.id[7])
+          currentGrid[squareNum] = "x"
+
+          clickCount++;
 				} else if (clickCount % 2 !== 0 && square.innerHTML === "") {
 					console.log(square.innerHTML);
 					//use O picture
 					square.innerHTML = `<img src="https://assets.aaonline.io/Module-DOM-API/formative-project-tic-tac-toe/player-o.svg" id="x-${clickCount}">`;
-					square.classList.add("o");
-					clickCount++;
+          square.classList.add("o");
+          let squareNum = (square.id[7])
+          currentGrid[squareNum] = "o"
+
+          clickCount++;
 				}
 			});
-		});
-	};
+    });
 
-	clickTracker();
+  };
+
+
+	playerClicks();
 });
